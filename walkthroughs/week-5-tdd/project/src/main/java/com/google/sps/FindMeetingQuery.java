079b31fe9c387a02a8fbfb9104e3d06cffe3d6f2
Zach Irving <zirving@lion.lmu.edu>
2020-06-04 20:49:47 +0000

    Initial work on FindMeeting

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -16,8 +16,47 @@ package com.google.sps;
 
 import java.util.Collection;
 
+private Collection<TimeRange> validTimes; 
+private Collection<TimeRange> invalidTimes; 
+
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+
+    HashSet<Event> meetings = events;
+    HashSet<String> attendees = request.getAttendees();
+
+    if(attendees.isEmpty()) {
+        TimeRange wholeDay = TimeRange.WHOLE_DAY;
+        HashSet<TimeRange> noAttendees = new HashSet<TimeRange>();
+        noAttendees.add(wholeDay);
+        return noAttendees; 
+    }
+    if(request.getDuration() > TimeRange.WHOLE_DAY.duration()){
+        return new HashSet<TimeRange>();
+    }
+
+    
+
+    for(Event meeting : meetings){
+        if(containsSharedAttendees(meeting.getAttendees(), attendees)) {
+            invalidTimes.addTime(meeting.getWhen());
+        }
+    }
   }
+  
+  /** @return true if the two sets contain any shared attendees. 
+   * Uses retainAll() to gather the intersection of the sets, and then checks if it is empty
+   */
+  public boolean containsSharedAttendees(Collection<String> attendeeSet1, Collection<String> attendeeSet2) {
+    attendeeSet1.retainAll(attendeeSet2);
+    if(attendeeSet1.isEmpty()){
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  /** 
+
+
 }
