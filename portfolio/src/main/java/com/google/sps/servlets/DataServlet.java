5cb376fb30c43eb17652329467ae0a76a30309e8
Zach Irving <zirving@lion.lmu.edu>
2020-06-04 21:01:24 +0000

    initial addition and work on sentiment and chart libraries before switch to week 5

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -20,13 +20,184 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.cloud.storage.Bucket;
+import com.google.cloud.storage.BucketInfo;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
+import com.google.api.gax.paging.Page;
+
+
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
 
+  public static final String COMMENT_DATA_KEY = "capitalizationData"; 
+  public static final String UPPERCASE_PROPERTY = "upperCase";
+  public static final String LOWERCASE_PROPERTY = "lowerCase";
+
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello world!</h1>");
+    response.setContentType("application/json;");
+
+    Query query = new Query("Comment").addSort("timestamp", SortDirection.DESCENDING);
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    PreparedQuery storedComments = datastore.prepare(query);
+
+    int commentLimit = getCommentLimit(request, storedComments.countEntities());
+    ArrayList<Comment> commentHistory = getCommentHistory(storedComments, commentLimit);
+    String json = convertToJsonUsingGson(commentHistory);
+    response.getWriter().println(json);
   }
+
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+      String username = getParameter(request, "username-input", "Anonymous");
+      String comment = getParameter(request, "comment-input", "");
+      long timestamp = System.currentTimeMillis();
+
+      Entity commentEntity = new Entity("Comment");
+      commentEntity.setProperty("username", username);
+      commentEntity.setProperty("comment", comment);
+      commentEntity.setProperty("timestamp", timestamp);
+
+      //updateCommentData(comment);
+
+      DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+      datastore.put(commentEntity);
+  
+      response.sendRedirect("/index.html#comment-section");
+
+  }
+
+  static void authImplicit() {
+  // If you don't specify credentials when constructing the client, the client library will
+  // look for credentials via the environment variable GOOGLE_APPLICATION_CREDENTIALS.
+  Storage storage = StorageOptions.getDefaultInstance().getService();
+
+  System.out.println("Buckets:");
+  Page<Bucket> buckets = storage.list();
+  for (Bucket bucket : buckets.iterateAll()) {
+    System.out.println(bucket.toString());
+  }
+ }
+  
+  private String convertToJsonUsingGson(Object data) {
+    Gson gson = new Gson();
+    String json = gson.toJson(data);
+    return json;
+  }
+
+  /**
+   * @return the request parameter, or the default value if the parameter
+   *         was not specified by the client
+   */
+  private String getParameter(HttpServletRequest request, String name, String defaultValue) {
+    String value = request.getParameter(name);
+    if (value == null) {
+      return defaultValue;
+    }
+    return value;
+  }
+
+  /**
+   * @return the requested number of comments to be shown, or, if none is requested, 
+   * the total number of comments in the comment history. 
+   */
+  private int getCommentLimit(HttpServletRequest request, int total) {
+    String requestedCommentLimit = getParameter(request, "comment-limit", "All");
+    int commentLimit;
+    if(requestedCommentLimit.equals("All")){
+      commentLimit = total;
+    } else {
+      commentLimit = Integer.parseInt(requestedCommentLimit);
+    }
+    return commentLimit; 
+  }
+
+  /**
+   * @return An ArrayList containing the commentHistory up to the given commentLimit
+   */
+  private ArrayList<Comment> getCommentHistory(PreparedQuery storedComments, int commentLimit) {
+    ArrayList<Comment> commentHistory = new ArrayList<Comment>(); 
+    for(Entity comment : storedComments.asIterable()){
+        commentHistory.add(new Comment(
+          (String) comment.getProperty("username"),
+          (String) comment.getProperty("comment")));
+
+        commentLimit--;
+        if(commentLimit <= 0){
+            break;
+        }
+    }
+    return commentHistory;
+  }
+
+  /**
+   * TODO: Updates the sentiment analysis data for the comment section (currently uses a placeholder)
+   
+  private void updateCommentData(String comment) {
+    Query query = new Query("CommentData");
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    PreparedQuery data = datastore.prepare(query);
+
+    Entity commentData = handleRetrievedData(PreparedQuery data); 
+
+    int[] capitalizationData = analyzeComment(String comment); 
+    int numLower = commentData.getProperty(LOWERCASE_PROPERTY) + capitalizationData[0];
+    int numUpper = commentData.getProperty(UPPERCASE_PROPERTY) + capitalizationData[1];
+
+    createCommentDataEntity(numLower, numUpper);
+  }
+
+  /**
+  * @return TODO: an array containing the sentiment data to be stored (currently uses a placeholder)
+  
+  private int[] analyzeComment(String comment) {
+    if(comment.charAt(0).isUpperCase()) {
+      return new int[] {0,1};
+    } else {
+      return new int[] {1,0};
+    }
+  }
+  
+  /** It is possible that updateCommentData will be validly called but somehow:
+   * 1. There does not exist any commentData entity OR
+   * 2. There are more than one commentData entities 
+   * This method handles either occurence. 
+   
+  private Entity handleRetrievedData(PreparedQuery data) {
+    Entity commentData; 
+    try {
+      commentData = data.asSingleEntity();
+    }
+    catch(TooManyResultsException e) {
+      data = handleExtraResults(PreparedQuery data);
+      commentData = data.asSingleEntity();
+    }
+    finally {
+      return commentData; 
+    }
+  }
+
+ /** Removes any results that are found that are not the correct commentData entity, 
+  * as determined by the correctDataKey 
+  
+  private PreparedQuery handleExtraResults(PreparedQuery data) {
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    for(Entity dataPoint : data.asIterable()){
+      if(dataPoint.getKey() != COMMENT_DATA_KEY){
+        datastore.delete(dataPoint.getKey());
+        }
+    }
+  }
+
+  private void createCommentDataEntity(int numLower, int numUpper) {
+    Entity commentDataEntity = new Entity("CommentData", COMMENT_DATA_KEY);
+    commentDataEntity.setProperty(LOWERCASE_PROPERTY, numLower);
+    commentDataEntity.setProperty(UPPERCASE_PROPERTY, numUpper);
+    
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    datastore.put(commentDataEntity);
+  }
+*/
 }
+
--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,17 +12,104 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/**
- * Adds a random greeting to the page.
- */
-function addRandomGreeting() {
-  const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
 
-  // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
+var DEFAULT_COMMENT_LIMIT = '10'; 
+
+/**
+ * Adds a random quote to the page.
+ */
+function addRandomQuote() {
+  const quotes =
+      ['"Hakuna Matata" - Timon and Pumbaa', '"The Dude abides." - The Dude', 
+      '"Strange women lying in ponds distributing swords is no basis for a system of government." - Dennis',
+      "\"I'm sorry Dave, I'm afraid I can't do that.\" - HAL 9000", '"Very nice." - Borat'];
+
+  // Pick a random quote.
+  const chosenQuote = quotes[Math.floor(Math.random() * quotes.length)];
 
   // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
+  const quoteContainer = document.getElementById('quote-container');
+  quoteContainer.innerText = chosenQuote;
 }
+
+/**
+ * Loads a music video from a clicked band. 
+ */
+function loadMusicVideo(){
+  const videoContainer = document.getElementById('video-container');
+  videoContainer.empty(); 
+  videoContainer.append("https://www.youtube.com/watch?v=-wVWjl9Kq6U");
+
+}
+
+function displayComments(commentLimit){
+  if(typeof commentLimit === 'undefined'){
+      commentLimit = DEFAULT_COMMENT_LIMIT;
+  }
+  const url = '/data?comment-limit=' + commentLimit;
+  fetch(url).then(response => response.json()).then((commentHistory) => {
+    const commentSection = document.getElementById("existing-comments");
+    if(commentHistory.length == 0) {
+      commentSection.innerHTML = " <p> No comments yet. Leave yours!</p>";
+      return;
+    } else {
+      document.getElementById("existing-comments").innerHTML = "";
+      for(i = 0; i<commentHistory.length;i++){
+        createCommentElement(commentHistory[i].username, commentHistory[i].content);
+      }
+    }
+  });
+}
+
+function updateCommentLimit(){
+  const commentLimit = document.getElementById("comment-limit-selector").value; 
+  displayComments(commentLimit);
+}
+
+function deleteComments(){
+    const method = {method : 'POST'};
+    fetch('/delete-data', method).then(displayComments());
+}
+
+function createCommentElement(user, comment){
+  const commentElement = document.createElement('div');
+
+  const username = document.createElement('h3');
+  username.appendChild(document.createTextNode(user));
+
+  const content = document.createElement('p');
+  content.appendChild(document.createTextNode(comment));
+
+  const divider = document.createElement('hr');
+
+  commentElement.appendChild(username);
+  commentElement.appendChild(content);
+  commentElement.appendChild(divider);
+
+  document.getElementById("existing-comments").appendChild(commentElement);
+}
+
+/** Creates a chart and adds it to the page. */
+function drawChart() {
+  const data = new google.visualization.DataTable();
+  data.addColumn('string', 'Animal');
+  data.addColumn('number', 'Count');
+        data.addRows([
+          ['Lions', 10],
+          ['Tigers', 5],
+          ['Bears', 15]
+        ]);
+
+  const options = {
+    'title': 'Zoo Animals',
+    'width':500,
+    'height':400
+  };
+
+  const chart = new google.visualization.PieChart(
+      document.getElementById('chart-container'));
+  chart.draw(data, options);
+}
+
